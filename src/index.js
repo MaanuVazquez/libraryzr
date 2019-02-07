eac05d466832649096d44e26a1685fe6ec41c41d
MaanuVazquez <maanuvazquez@gmail.com>
2019-02-07 00:10:14 -0300

    add husky

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ window.addEventListener('storage-changed', () => {
 
 window.localStorage.setItem('a', 'a')
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
